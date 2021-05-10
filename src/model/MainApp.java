ca24f5f21645667c62c79a9b53751695e5130d6d
Walid Aloui <aloui.walid@outlook.fr>
2021-05-10 14:29:29 +0200

    Ajout d'un menu

--- src/model/MainApp.java
+++ src/model/MainApp.java
@@ -2,11 +2,14 @@ package model;
 
 import java.sql.SQLException;
 
+import ui.CommandLineInterface;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		try {
 			Database db = Database.create();
+			CommandLineInterface.ShowMenu();
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
---
+++ src/ui/CommandLineInterface.java
@@ -0,0 +1,38 @@
+package ui;
+
+import java.sql.SQLException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import model.Database;
+
+public class CommandLineInterface {
+
+	// Methode qui permet d'afficher le menu
+
+	public static void ShowMenu() throws SQLException {
+		Scanner sc = new Scanner(System.in);
+		short opt = 0;
+		while (opt != 7) {
+			do {
+				System.out.println("\nVeuillez selectionner l'option souhaite");
+				System.out.println("1- Afficher la liste des fabriquants");
+				System.out.println("2- Afficher la liste des ordinateurs");
+				System.out.println("3- Afficher les details d'un ordinateur");
+				System.out.println("4- Mettre a jour un ordinateur");
+				System.out.println("5- Creer un ordinateur");
+				System.out.println("6- Supprimer un ordinateur");
+				System.out.println("7- Quitter\n");
+				try {
+					opt = sc.nextShort();
+				}catch(InputMismatchException e) {
+					System.out.println("Veuillez entrez un chiffre !");
+					ShowMenu();
+				}
+				if (opt < 1 || opt > 7)
+					System.out.println("Option invalide !");
+			} while (opt < 1 || opt > 7);
+		}
+	}
+
+}
